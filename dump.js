e590f3621f492a9cc5e87f22eb68f2a059fc2d1c
smart-stuff-innit <109387268+smart-stuff-innit@users.noreply.github.com>
2022-07-15 20:57:11 +0100

    Update dump.js
    Add generate_canvas

--- dump.js
+++ dump.js
@@ -1,1 +1,13 @@
-
+function generate_canvas(layer) {
+	c = document.createElement("canvas");
+	c.style.width='100%';
+	c.style.height='100%';
+	c.width = window.innerWidth;
+	c.height = window.innerHeight;
+	c.style.position='absolute';
+	c.style.left=0;
+	c.style.top=0;
+	c.style.zIndex = layer;
+	document.body.appendChild(c);
+	return c
+}
